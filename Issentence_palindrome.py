88f1d1ad06bc8fd6c4a0848cc594fb9a54140e71
Adesayo M <105006406+Adesayo20@users.noreply.github.com>
2024-09-27 05:12:49 +0100

    Update Issentence_palindrome.py

--- Issentence_palindrome.py
+++ Issentence_palindrome.py
@@ -1,23 +1,22 @@
 def is_palindrome(string):
     return string[::-1].casefold() == string.casefold()
 
-
 def is_palindrome2(sentence):
     """
-    States if a string is a palindrome
-    :param sentence:The string that the user types in when prompted to enter the value
+    States if a `string` is a palindrome
+    :param sentence:The `string` that the user types in when prompted to enter the value
     :return: Returns a boolean vale
     """
     string=""
     for char in sentence:
         if char.isalnum():
             string+=char
-        return is_palindrome(string)
+    return is_palindrome(string)
         # sentence=''.join([char for char in sentence if char.isalpha()])
         # return sentence[::-1].casefold() == sentence.casefold()
 
 word=input("Write a sentence")
 if is_palindrome2(word):
-    print("That sentence is a palindrome")
+    print(f"{word} is a palindrome")
 else:
-    print("That sentence is NOT a palindrome")
+    print(f"{word}is Not a palindrome")
